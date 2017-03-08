9647d8135e98a935f35f92ed7d146548a9dfd285
Sara Bahrani <Sara.Bahrani@gmail.com>
2017-03-07 21:28:53 -0800

    Make changes to connect to store and read from store

--- src/containers/base/CheckBox.js
+++ src/containers/base/CheckBox.js
@@ -3,28 +3,54 @@ import { connect } from 'react-redux';
 import Checkbox from 'material-ui/Checkbox';
 import { emailNotification } from '../../actions';
 class CheckBox extends React.Component {
+    constructor(props) {
+        //  console.log('constructor');
+        super(props);
+        this.state = {
+            value: props.value || false,
+        };
+    }
+    componentWillReceiveProps(nextProps) {
 
-    check = (e) => {
-        this.props.emailNotification(e.target.checked,e.target.name);
+        if (this.props.value != nextProps.value) {
+            if (this.state.value != nextProps.value)
+                this.setState({ value: nextProps.value });
+        }
+    }
+
+    check = () => {
+        // console.log('check');
+        const value = !this.state.value;
+        this.setState({ value });
+        this.props.checked(value, this.props.name);
     }
     render() {
         const p = this.props;
-
+        //console.log('render');
         return (
             <Checkbox name={p.name}
                 label={p.label}
-                defaultChecked={p.checked}
+                checked={this.state.value}
                 onCheck={this.check}
-                />
+            />
         );
     }
 }
-CheckBox.propTyps = {
-    emailNotification: PropTypes.func,
+CheckBox.propTypes = {
+    name: PropTypes.string.isRequired,
+    value: PropTypes.bool,
+    checked: PropTypes.func,
 };
+function mapStateToProps(state, props) {
+    return {
+        value: state.form && state.form[props.name],
+    };
+
+}
+
 const mapDispatchToProps = (dispatch) => {
     return {
-        emailNotification: (checked,name) => dispatch(emailNotification(checked,name)),
+        checked: (value, name) => dispatch({ type: 'FORM', value, name }),
     };
 };
-export default connect(null, mapDispatchToProps)(CheckBox);
+export default connect(mapStateToProps, mapDispatchToProps)(CheckBox);
