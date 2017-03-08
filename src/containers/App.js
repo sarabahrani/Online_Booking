af4b6f323fc1f4d3b966bd9471a519318069a696
Sara Bahrani <Sara.Bahrani@gmail.com>
2017-03-07 21:27:30 -0800

    The highest-level of app

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
-
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 //import Divider from 'material-ui/Divider';
 
 class App extends React.Component {
     render() {
         return (
-            <div>
-                <div>Header</div>
-                <div className="container">{this.props.children}</div>
-                <div>Footer</div>
-            </div>
+            <MuiThemeProvider>
+                <div>
+                    <div className="container">Header</div>
+                    <hr />
+                    <div className="container-fluid">{this.props.children}</div>
+                    <hr />
+                    <div className="container">Footer</div>
+                </div>
+            </MuiThemeProvider>
         );
     }
 }
