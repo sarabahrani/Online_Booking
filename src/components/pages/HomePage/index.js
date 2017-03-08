d6fad7f17df2cecb6b09881e6d172a931fbe4278
Sara Bahrani <Sara.Bahrani@gmail.com>
2017-03-07 21:26:21 -0800

    Make changes in style

--- src/components/pages/HomePage/index.js
+++ src/components/pages/HomePage/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Calendar from '../../../containers/enhanced/Calendar';
 import AppointmentDetail from '../../enhanced/AppointmentDetail';
 
@@ -8,12 +7,14 @@ import AppointmentDetail from '../../enhanced/AppointmentDetail';
 class HomePage extends React.Component {
     render() {
         return (
-            <MuiThemeProvider>
-                <form>
+            <div>
+                <div className="container-fluid col-md-6">
                     <Calendar clientId="1" date={new Date()} />
+                </div>
+                <div className="container col-md-5 offset-md-1">
                     <AppointmentDetail />
-                </form>
-            </MuiThemeProvider>
+                </div>
+            </div>
         );
     }
 }
