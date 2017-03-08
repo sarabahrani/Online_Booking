44d788dd62e2216ab26f6dea2ede662f5e083b58
Sara Bahrani <Sara.Bahrani@gmail.com>
2017-03-07 21:25:02 -0800

    -Set the property of component in store -Add reset and submit button

--- src/components/enhanced/AppointmentDetail.js
+++ src/components/enhanced/AppointmentDetail.js
@@ -6,15 +6,28 @@ import CheckBox from '../../containers/base/CheckBox';
 class AppointmentDetail extends React.Component {
     render() {
         return (
-            <div>
-                <TextBox name="FullName" required />
-                <PhoneBox name="Phone" required />
-                <EmailBox name="Email" />
-                <h4>Email Notification Schedule</h4>
-                <CheckBox name="week" label="One week later" />
-                <CheckBox name="day" label="One day Later" checked />
-                <CheckBox name="hour" label="2 hours later" />
-            </div>);
+            <form>
+                <div>
+                    <TextBox name="FullName" required />
+                    <PhoneBox name="Phone" required />
+                    <EmailBox name="Email" />
+                </div>
+                <br />
+                <div>
+                    <h4>Email Notification Schedule</h4>
+                    <CheckBox name="week" label="One week later" />
+                    <CheckBox name="day" label="One day Later"  />
+                    <CheckBox name="hour" label="2 hours later" />
+                </div>
+                <br />
+                <div>
+                    <div>
+                        <button type="reset" className="btn">Cancel</button>
+                        <button type="submit" className="btn btn-info">Submit</button>
+                    </div>
+                </div>
+            </form>
+        );
     }
 }
 export default AppointmentDetail;
